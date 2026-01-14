a50e342450fbaab5c45942cb54e30b52616b1677
Mahesh Kotakonda <mahesh04815@gmail.com>
2026-01-14 09:22:24 +0530

    Update index.js

--- app/backend/index.js
+++ app/backend/index.js
@@ -29,6 +29,8 @@ app.post("/api/notes", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Backend running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on 0.0.0.0:${PORT}`);
 });
+
+
